828772d08e0bfc8d290896f970dca2db1516abf7
Viet Duc Kieu <viet.duc.kieu@web.de>
2022-01-22 12:52:07 +0100

    setup post endpoint to receive image (not evaluated yet)

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 
@@ -11,6 +13,21 @@ class HelloWorld(Resource):
 
 
 api.add_resource(HelloWorld, "/helloWorld")
+
+
+image_post_args = reqparse.RequestParser()
+image_post_args.add_argument(
+    "name", type=str, help="you didnt send name", required=True)
+
+
+class UploadImage(Resource):
+    def post(self):
+        args = image_post_args.parse_args()
+        print(args.name)
+        return{"data": "post works now 2"}
+
+
+api.add_resource(UploadImage, "/uploadImage")
 
 
 if __name__ == "__main__":
