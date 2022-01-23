b4042898c765eab16e79ed2633f7929e456e241e
Viet Duc Kieu <viet.duc.kieu@web.de>
2022-01-23 23:32:56 +0100

    added emotion detection

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS
+from fer import FER
+import cv2
+import base64
 
 app = Flask(__name__)
 CORS(app)
@@ -17,14 +20,21 @@ api.add_resource(HelloWorld, "/helloWorld")
 
 image_post_args = reqparse.RequestParser()
 image_post_args.add_argument(
-    "name", type=str, help="you didnt send name", required=True)
+    "encodedImage", type=str, help="you didnt send encoded base64 image", required=True)
 
 
 class UploadImage(Resource):
     def post(self):
         args = image_post_args.parse_args()
-        print(args.name)
-        return{"data": "post works now 2"}
+        img_base64 = args.encodedImage
+        img_base64_no_metadata = img_base64.split(",")[-1]
+        with open("imageToSave.png", "wb") as fh:
+            fh.write(base64.b64decode(img_base64_no_metadata))
+        img = cv2.imread("imageToSave.png")
+        detector = FER(mtcnn=True)
+        emotion, score = detector.top_emotion(img)
+
+        return{"emotion": emotion, "score": score}
 
 
 api.add_resource(UploadImage, "/uploadImage")
